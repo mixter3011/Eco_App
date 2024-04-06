2c5b226a3a2cf72c30afed0d59a4365ea30a0665
fobic <sabychakraborty08@gmail.com>
2024-04-06 21:13:00 +0530

    func(quota):addded a post request

--- lib/pages/quota.dart
+++ lib/pages/quota.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:http/http.dart' as http;
 
 class QuotaPage extends StatefulWidget {
   const QuotaPage({Key? key}) : super(key: key);
@@ -40,6 +43,48 @@ class _QuotaPageState extends State<QuotaPage> {
     _updateEmissions(selectedDate);
   }
 
+  void sendPUTRequest() async {
+    final url = Uri.parse('http://localhost:8181');
+
+    final List<Map<String, dynamic>> listOfObjects = [
+      {'2024-03-22': '79', '2024-03-23': '77'},
+      {'2024-03-24': '77', '2024-03-25': '75'},
+      {'2024-03-26': '80', '2024-03-27': '82'},
+      {'2024-03-28': '84', '2024-03-29': '72'},
+      {'2024-03-30': '79', '2024-03-31': '78'},
+      {'2024-04-01': '83', '2024-04-02': '80'},
+      {'2024-04-03': '77', '2024-04-04': '78'},
+      {'2024-04-05': '77', '2024-04-06': '76'},
+    ];
+
+    final usage = json.encode({'arrayname': listOfObjects});
+
+    try {
+      final response = await http.put(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: {usage},
+      );
+
+      if (response.statusCode == 200) {
+        print('PUT Request Succesful');
+        print('Response: ${response.body}');
+      } else {
+        print('Failed to send PUT request');
+        print('Response status code: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      }
+    } catch (e) {
+      print('Error sending PUT request: $e');
+    }
+  }
+
+  void main() {
+    sendPUTRequest();
+  }
+
   // padding constants
   final double verticalPadding = 25;
 
@@ -54,7 +99,7 @@ class _QuotaPageState extends State<QuotaPage> {
     ["2024-03-29", 18, 19, 15, 20],
     ["2024-03-30", 15, 24, 23, 17],
     ["2024-03-31", 16, 22, 17, 23],
-    ["2024-04-01", 23, 18, 20, 22], 
+    ["2024-04-01", 23, 18, 20, 22],
     ["2024-04-02", 24, 16, 21, 19],
     ["2024-04-03", 17, 20, 16, 24],
     ["2024-04-04", 20, 24, 19, 15],
@@ -150,6 +195,7 @@ class _QuotaPageState extends State<QuotaPage> {
 
   @override
   Widget build(BuildContext context) {
+    String co2Usage = (calculatetotalCO2usage() * 100).toStringAsFixed(2);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -161,25 +207,49 @@ class _QuotaPageState extends State<QuotaPage> {
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.background,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Text(
-              "DAILY QUOTA: 100 UNITS",
-              style: TextStyle(
-                  fontFamily: "Poppins",
-                  fontWeight: FontWeight.bold,
-                  fontSize: 34),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 160),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 8.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '$co2Usage% of Quota Hit',
+                  style: const TextStyle(
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
                 ),
+                const SizedBox(height: 8.0),
+                Text(
+                  _getCurrentDateText(DateTime.now()),
+                  style: const TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 20,
+                  ),
+                ),
+                const Text(
+                  "Quota Limit: 100",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
                 child: LinearPercentIndicator(
                   animation: true,
+                  width: MediaQuery.of(context).size.width - 20,
                   animationDuration: 1000,
                   lineHeight: 20,
                   percent: calculatetotalCO2usage(),
@@ -188,15 +258,36 @@ class _QuotaPageState extends State<QuotaPage> {
                 ),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Divider(
+              thickness: 1,
+              color: Color.fromARGB(255, 204, 204, 204),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+                child: Text(
+                  "Daily log",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
                   padding: const EdgeInsets.only(bottom: 120),
                   child: HeatMap(
-                    startDate: DateTime.now().subtract(const Duration(days: 14)),
-                    endDate: DateTime.now().add(const Duration(days: 40)),
-                    margin: const EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                    startDate:
+                        DateTime.now().subtract(const Duration(days: 14)),
+                    endDate: DateTime.now().add(const Duration(days: 20)),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 7, vertical: 5),
                     datasets: _generateHeatmapDataset(),
                     colorMode: ColorMode.opacity,
                     showText: true,
@@ -215,10 +306,10 @@ class _QuotaPageState extends State<QuotaPage> {
                     },
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
